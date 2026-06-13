2e7f503c3e16ebc2080c60fc91bd3a46317280ba
Vaishnavi Deepak Morankar <vaishnavimorankar296@gmail.com>
2026-06-13 20:24:18 +0530

    Add files via upload

---
+++ Python/Logic_Building/Practice/SequenceX.py
@@ -0,0 +1,17 @@
+# Sequence data type
+
+Data1 = [10, 20, 30,40]                                # List
+Data2 = (10, 20, 30,40)                                # Tuple
+Data3 = {10, 20, 30,40}                                # Set
+Data4 = {"A" : 10, "B" : 20, "C" : 30, "D" : 40}       # Dict
+Data5 = "Maharashtra"                                  # Str
+
+print(type(Data1))
+print(type(Data2))
+print(type(Data3))
+print(type(Data4))
+print(type(Data5))
+
+# jr ordered data asel tr te index astatch
+# jr unordered data asel tr index nasat
+
