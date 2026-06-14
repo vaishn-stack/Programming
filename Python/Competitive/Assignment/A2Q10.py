b515b1557270f1fbe85445cd5611e880b2843560
Vaishnavi Deepak Morankar <vaishnavimorankar296@gmail.com>
2026-06-15 01:58:27 +0530

    Add files via upload

---
+++ Python/Competitive/Assignment/A2Q10.py
@@ -0,0 +1,5 @@
+# what will be the output:
+
+print("10" + "20")
+
+print(10 + 20)
---
+++ Python/Competitive/Assignment/A2Q9.py
@@ -0,0 +1,6 @@
+# Write a program to take users name and age and display: Hello <name>, you will turn <age+1> next year.
+
+name = input("Enter your Name : ")
+age = int(input("Enter your age : "))
+
+print("Hello", name + ", you will turn", age + 1, "next year.")
